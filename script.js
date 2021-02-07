64264c5400b7139bd520f72f8a73f5fccc27c985
Mohammad Rouf <mrouf82@gmail.com>
2021-02-06 22:51:40 -0500

    fixed broken code

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function setDate() {
 setDate();
 //runs the setDate function every second so the second can be changing without an reloading the screen
 var intervalID = window.setInterval(setDate, 1 * 1000);
-var iconCard = document.getElementById("iconCard");
+
 var displayCity = document.getElementById("inputed-city");
 var temperature = document.getElementById("temperature");
 var humidity = document.getElementById("humidity");
@@ -32,16 +32,63 @@ var proxy = "http://cors-anywhere.herokuapp.com/";
 var apiKey = "b15bd32eadfe77cf8c5d312f33bcb3f8";
 function weather(cityName) {
   fetch(
-    `${proxy}api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
   )
     .then(function (data) {
       return data.json();
     })
     .then(function (response) {
       var iconPic = response.weather[0].icon;
-      var iconUrl = `http://openweathermap.org/img/wn/${iconPic}@2x.png`;
-      document.getElementById("city").textContent = `${cityName}`;
-      $(iconCard).html("<img src=" + iconUrl + ">");
+      var iconUrl = "https://openweathermap.org/img/wn/" + iconPic + "@2x.png";
+      // var image = document.getElementById("iconCard");
+      var city = document.getElementById("city");
+      // document.getElementById("city").textContent = `${cityName}`;
+      // image.src = iconUrl;
+      $(city).html(response.name + "<img src=" + iconUrl + ">");
+      temperature.textContent = response.main.temp;
+      humidity.textContent = response.main.humidity + "%";
+      windspeed.textContent = response.wind.speed + "mph";
+      findUV(response.coord.lon, response.coord.lat);
+      forcast(cityName);
     });
 }
-// ${response.main.temp}  F
+function findUV(lon, lat) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${apiKey}`
+  )
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (response) {
+      uvIndex.textContent = response.current.uvi;
+    });
+}
+
+function forcast(cityName) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+  )
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (response) {
+      for (i = 0; i < 5; i++) {
+        var date = new Date(
+          response.list[(i + 1) * 8 - 1].dt * 1000
+        ).toLocaleDateString();
+        var iconcode = response.list[(i + 1) * 8 - 1].weather[0].icon;
+        var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+        var temp = (
+          (response.list[(i + 1) * 8 - 1].main.temp - 273.5) * 1.8 +
+          32
+        ).toFixed(2);
+        var humidity = response.list[(i + 1) * 8 - 1].main.humidity;
+        document.getElementById("date" + i).textContent = date;
+        document.getElementById("forcastImg" + i).innerHTML =
+          "<img src=" + iconurl + ">";
+        document.getElementById("temp" + i).innerHTML = temp + "&#8457";
+        // document.getElementById("fHumidity" + i).innerHTML = fhumidity + "%";
+        document.getElementById("fHumidity" + i).textContent = humidity + "%";
+      }
+    });
+}
